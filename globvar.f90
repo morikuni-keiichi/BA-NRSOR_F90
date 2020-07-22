cc8fb342aba6e5929f176b09f622a86042e5105a
Keiichi MORIKUNI <morikuni.keiichi@gmail.com>
2020-07-22 11:22:33 +0900

    Delete globvar.f90
    Deleted globvar.f90

--- globvar.f90
+++
@@ -1,5 +0,0 @@
-module globvar
-  intrinsic :: selected_real_kind
-  integer,  parameter, public :: dp = selected_real_kind(15)
-  real(dp), parameter, public :: zero = 0.0_dp, one = 1.0_dp
-end module globvar
