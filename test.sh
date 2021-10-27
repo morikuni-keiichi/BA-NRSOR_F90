705f71d9f56d42ef3de83b40698fb358918db606
Keiichi MORIKUNI <morikuni.keiichi@gmail.com>
2021-10-27 11:44:49 +0900

    modified test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make
-./main --at=1 --nin=50 --omg=1.0 --tol=1.0e-8 --omax=800 --rmax=0 --output_mode=0 --fi=RANDL7/
+./main --at=1 --nin=50 --omg=1.0 --tol=1.0e-8 --omax=800 --rmax=0 -v --directory=RANDL7/
 gnuplot plot.plt
